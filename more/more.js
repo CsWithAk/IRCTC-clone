9f8848d522ee93899ef6a3eafa3ae6facba8b962
Amit Kumar <amitkumarmjl22@gmail.com>
2025-04-17 19:04:23 +0530

    Update more.js

--- more/more.js
+++ more/more.js
@@ -1,6 +1,6 @@
 /*======= For Login at Home page ========*/
 function back(){
-    alert("Are you  sure want to back!");
+    alert("Are you sure want to back!");
     window.location.href='index.html';
 }
 /*======= For Login at Home page ========*/
